1525bd183be98164e364b110227da83c8d4f8190
YanSteinhaus <yanvsteinhaus@gmail.com>
2024-11-07 18:47:29 -0300

    rotation/animation
    -enabled rotation to characters
    -AnimatorControlerScripts
    -fixed slide on movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody Rigidbody;
+    private float horizontal;
+    private float moveSpeed;
+
+
+    void Start()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+        moveSpeed = 15f;
+
+    }
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            
+            Rigidbody.linearVelocity = new Vector2(horizontal * moveSpeed, Rigidbody.linearVelocity.y);
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+
+           Rigidbody.linearVelocity = new Vector2(horizontal * moveSpeed, Rigidbody.linearVelocity.y);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+           
+
+    }
+}
